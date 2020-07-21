649b3f16febd5da737295c306b584223926db564
buihang2501 <buihang2501@gmail.com>
2020-07-21 14:42:42 +0700

    import text

---
+++ app/src/main/java/com/co_well/quiz/ui/activity/ImportTextActivity.kt
@@ -0,0 +1,11 @@
+package com.co_well.quiz.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImportTextActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_import_text)
+    }
+}
--- app/src/main/java/com/co_well/quiz/ui/activity/interf/OnTextClick.kt
+++
@@ -1,5 +0,0 @@
-package com.co_well.quiz.ui.activity.interf
-
-interface OnTextClick {
-    fun textClick(string: String)
-}
